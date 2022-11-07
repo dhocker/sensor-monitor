306be4441974dfcfc8637d2bb106a7cf93b1c4f5
Dave Hocker <AtHomeX10@gmail.com>
2022-11-07 11:56:48 -0600

    Update copyright date

--- sensor_thread.py
+++ sensor_thread.py
@@ -1,6 +1,6 @@
 #
 # sensor_thread.py - RuuviTag sensor data collection
-# Copyright © 2019 Dave Hocker
+# Copyright © 2022 Dave Hocker
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
